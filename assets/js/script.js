7949898cc7b58f7eeb92da8acbf088fd4e395a2e
smacdonald97 <smacdonald97@email.arizona.edu>
2022-01-23 09:11:04 -0700

    styled the footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 window.alert("Test Your Might!")
 
-var questionArray 
+var questionArray = [
+    {question: "How do you write a comment in Javascript?",
+    answers: {
+        a: "// and /* */",
+        b: "// only",
+        c: "/* */ only",
+        d: "none of the above"
+    },
+    correctAnswer: "a"
+},
+{
+    question: "Which data type is a string?",
+    answers: {
+        a: "the quick brown fox jumps over the lazy dog",
+        b: "42",
+        c: "x = {lastName: 'Smith', ageRange: '18-25'}",
+        d: "none of the above"
+    },
+    correctAnswer: "a"
+}
+]
